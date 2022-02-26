6a4ba5e2f7c7d632080b5f700cbf9ae3a420bc45
Toru-ymgc <jimmy33hiza@yahoo.co.jp>
2022-02-26 18:48:43 +0900

    イベントタイトルのみスレッドに投稿する機能追加

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 require('dotenv').config();
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+global.fetch = require('node-fetch');
 
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+let today = new Date();
+let thisYear = today.getFullYear();
+let thisMonth = ("00"+(today.getMonth())).slice(-2);
+let nextMonth = ("00" + (today.getMonth()+1)).slice(-2);
+let heldThisMonth = String(thisYear)+String(thisMonth)
+let heldNextMonth = String(thisYear)+String(nextMonth)
+
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.channels.cache.filter(ch => ch.name === '独り言').forEach(ch => ch.send('オンラインになりました。\n::+文字入力でeventinfobotが起動します。'))
-    var request = new XMLHttpRequest();
-    let url='https://connpass.com/api/v1/event/?keyword=テスト自動化&ym=202112'
-    let encodeUrl = encodeURI(url);
-    request.open('GET', encodeUrl, true);
-    request.responseType = 'json';
-    request.onload = function () {
-        // レスポンスが返ってきた時の処理 ここにメッセージを表示させるのかな
-        client.channels.cache.filter(ch => ch.name === '独り言').forEach(ch => ch.send('API反応したよ'))
+    client.channels.cache.filter(ch => ch.name === '独り言').forEach(ch => ch.send('オンラインになりました。\n::+eventinfobotを起動します。'))
+    async function callApi(){
+        let url='https://connpass.com/api/v1/event/?keyword=テスト自動化&Count=10&ym='+heldThisMonth+'&ym='+heldNextMonth
+        let encodeUrl = encodeURI(url);
+        const res = await fetch(encodeUrl);
+        const jData = await res.json();
+        for (let i=0; i<10; i++){
+            client.channels.cache.filter(ch => ch.name === '独り言').forEach(ch => ch.send(jData.events[i].title));
+            // TODO : タイトルとイベントURLと開催日を追加
+            // TODO : チャンネルを限定する式を簡単化
+        }
     }
-    // リクエストをURLに送信
-    request.send();
+
+    callApi();
 });
 
 client.on('message', message => {
-    if(message.author.bot) return;
-    if ( message.content === "haruo world!") { 
-        message.reply("三波春夫でございます！");
-    }
+
+    // メッセージで検索できるようにしたい
+
 });
 
 client.login(process.env.DISCORD_TOKEN)
