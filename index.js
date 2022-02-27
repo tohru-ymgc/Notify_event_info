ecc56c2c9b412107f036f790ed5697c02b75e8b6
Toru-ymgc <jimmy33hiza@yahoo.co.jp>
2022-02-27 23:02:21 +0900

    検索結果数に基づいて、データを出力するようにした(最大10件)

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ global.fetch = require('node-fetch');
 
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// HACK : 日付部分もっとすっきりさせたい
 let today = new Date();
 let thisYear = today.getFullYear();
 let thisMonth = ("00"+(today.getMonth())).slice(-2);
@@ -10,28 +12,41 @@ let nextMonth = ("00" + (today.getMonth()+1)).slice(-2);
 let heldThisMonth = String(thisYear)+String(thisMonth)
 let heldNextMonth = String(thisYear)+String(nextMonth)
 
+async function callApi(){
+    // HACK : テスト自動化以外でも検索できるようにしたい
+    let url='https://connpass.com/api/v1/event/?keyword=テスト自動化&Count=10&ym='+heldThisMonth+'&ym='+heldNextMonth
+    let encodeUrl = encodeURI(url);
+    const res = await fetch(encodeUrl);
+    const jData = await res.json();
+    let max = 0;
+    if(jData.results_available > 10){
+        max = 10;
+    }else{
+        max = jData.results_available
+    }
+    console.log(max);
+
+    // HACK : メッセージに返す処理はcallAPIと分割させたい
+
+    for (let i=0; i<max; i++){
+        // TODO : 開催日が今日の日付より前ならメッセージに返さない処理を追加
+        // TODO : あるチャンネルに限定せずにメッセージを返したい
+        client.channels.cache.filter(ch => ch.name === '独り言').forEach(ch=>ch.send("\n-----"));
+        client.channels.cache.filter(ch => ch.name === '独り言').forEach(ch=>ch.send(jData.events[i].title));
+        client.channels.cache.filter(ch => ch.name === '独り言').forEach(ch=>ch.send(jData.events[i].started_at));
+        client.channels.cache.filter(ch => ch.name === '独り言').forEach(ch=>ch.send(jData.events[i].event_url));    
+    }
+}
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.channels.cache.filter(ch => ch.name === '独り言').forEach(ch => ch.send('オンラインになりました。\n::+eventinfobotを起動します。'))
-    async function callApi(){
-        let url='https://connpass.com/api/v1/event/?keyword=テスト自動化&Count=10&ym='+heldThisMonth+'&ym='+heldNextMonth
-        let encodeUrl = encodeURI(url);
-        const res = await fetch(encodeUrl);
-        const jData = await res.json();
-        for (let i=0; i<10; i++){
-            client.channels.cache.filter(ch => ch.name === '独り言').forEach(ch => ch.send(jData.events[i].title));
-            // TODO : タイトルとイベントURLと開催日を追加
-            // TODO : チャンネルを限定する式を簡単化
-        }
-    }
-
+    client.channels.cache.filter(ch => ch.name === '独り言').forEach(ch => ch.send('オンラインになりました。\nテスト自動化に関するイベントを通知します。'))
     callApi();
 });
 
 client.on('message', message => {
 
-    // メッセージで検索できるようにしたい
+    // NOTE : メッセージで検索できるようにしたい
 
 });
 
